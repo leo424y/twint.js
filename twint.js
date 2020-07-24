529e30d41cc9cdda6a896050be9fb254ad78a1aa
Fly Chang <leo424y@gmail.com>
2020-07-24 14:38:31 +0800

    first commit

---
+++ twint.js
@@ -0,0 +1,40 @@
+var express = require('express');
+const {
+  exec
+} = require('child_process');
+var fs = require('fs');
+
+var app = express();
+app.get("/", function (req, res) {
+  var q = req.query.q;
+  var date = req.query.date;
+
+  if (typeof q !== undefined && date !== undefined) {
+    twint(q,date);
+    res.send(`${q} ${date} were sent.`);
+  } else {
+    res.send('q=, and date= are required');
+  }
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
+
+function twint(q,date) {
+  var query = `twint -es 35.194.129.109:9200 -s ${q} --since "${date} 00:00:00" --limit 10`
+  exec(query, {
+    cwd: ''
+  }, (err, stdout, stderr) => {
+    if (err) {
+
+      // node couldn't execute the command
+      return;
+    }
+
+    // the *entire* stdout and stderr (buffered)
+    console.log(`${query}`);
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+  });
+}
